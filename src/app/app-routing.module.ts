87e5d24bd9888eddce900c6602d1863835ae8fe5
Anastasiia Olenchenko <an.olenchenko@gmail.com>
2022-04-22 09:59:12 +0200

    Add main components and routing for it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((mod) => mod.UserModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+
+const routes: Routes = [
+  {
+    path: 'list',
+    component: UserListComponent,
+  },
+  {
+    path: 'detail',
+    children: [
+      {
+        path: ':id',
+        component: UserDetailComponent,
+      }
+    ]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list',
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forChild(routes) ],
+  exports: [ RouterModule ]
+})
+export class UserRoutingModule {
+}
